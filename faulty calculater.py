b66329e150a125c9350e03d33790a134a68e2e40
mig-ashu <72605059+mig-ashu@users.noreply.github.com>
2020-10-22 02:52:45 +0530

    Add files via upload

---
+++ faulty calculater.py
@@ -0,0 +1,37 @@
+#faluty calculater
+ #45*3=555, 56+9=77, 56/6=4 faulty calculation
+
+print("This faulty calculater make by Ashish(Nirex)")
+
+print("chose oprater")
+print("+ for add ")
+print("- for subtract")
+print("* for multiply ")
+print("/ for divide ")
+
+while(True):
+    oprater=input("chose oprater:\n")
+
+    print("Enter first number :")
+    num1=int(input())
+    print("Enter secound number :")
+    num2=int(input())
+
+    if num1 is 45 and num2 is 3 and oprater is'*' : #faulty situation
+        print("Ans : ", 555)
+    elif num1 is 56 and num2 is 9 and oprater is '+':
+        print("Ans : " ,77)
+    elif num1 is 56 and num2 is 6 and oprater is '/':
+        print("Ans : ", 4)
+
+    elif oprater=='+': #correct situation
+        print("Ans : ", num1 + num2)
+    elif oprater == '-':
+        print("Ans : ", num1 - num2)
+    elif oprater == '*':
+        print("Ans : ", num1 * num2)
+    elif oprater == '/':
+        print("Ans : ", num1 / num2,"\n")
+
+    else:
+        print("unexpected")
